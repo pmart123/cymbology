ee59387b614efe89204e293587d0db9b2820c6c9
pmart123 <philip.martin2007@gmail.com>
2017-09-15 15:34:11 -0500

    Add version file and update repo setup.

--- setup.py
+++ setup.py
@@ -1,13 +1,50 @@
+#!/usr/bin/env python
+
+import os
 from os.path import exists
 from setuptools import setup
 
+ENCODING = 'utf-8'
+PACKAGE_NAME = 'cymbology'
+
+DESCRIPTION = 'financial identifier validation.'
+
+local_directory = os.path.abspath(os.path.dirname(__file__))
+version_path = os.path.join(local_directory, PACKAGE_NAME, '_version.py')
+
+version_ns = {}
+with open(version_path, 'r', encoding=ENCODING) as f:
+    exec(f.read(), {}, version_ns)
+
+
+def get_requirements(requirement_file):
+    requirements = list(
+        open(requirement_file, 'r',
+             encoding=ENCODING).read().strip().split('\r\n'))
+    return requirements
+
+
+def get_read_me(file_name):
+    return open(file_name).read() if exists(file_name) else DESCRIPTION
+
+
 setup(
-    name='security_id',
-    version='0.1',
-    packages=['security_id'],
-    url='https://github.com/pmart123/security_id',
+    name='PACKAGE_NAME',
+    packages=[PACKAGE_NAME],
+    description='financial identifier validation.',
+    package_data={
+        '': ['*.txt', '*.json'],
+    },
     license='BSD',
+    include_package_data=True,
+    version=version_ns['__version__'],
+    url='https://github.com/pmart123/cymbology',
     author='Philip Martin',
     author_email='philip.martin2007@gmail.com',
-    long_description=open('README.md').read() if exists("README.md") else 'Identify and Validate Financial Security Codes',
+    extras_require={
+        'develop': get_requirements('requirements-dev.txt'),
+        'test': get_requirements('requirements-test.txt')
+    },
+    long_description=get_read_me('README.md'),
+    zip_safe=False
 )
