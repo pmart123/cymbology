ab694ea7d9f4e9f0114a5bcdd2d4a09aa9e4d4ed
pmart123 <philip.martin2007@gmail.com>
2017-09-20 14:33:50 -0500

    Update exception names, documentation.

--- cymbology/exceptions.py
+++ cymbology/exceptions.py
@@ -4,19 +4,21 @@ class IdError(Exception): pass
 class NullError(IdError): pass
 
 
-class CharacterError(IdError): pass
+class InvalidCharacterError(IdError):
+    """Input contains invalid characters base on identifer specification."""
 
 
-class CheckDigitError(IdError): pass
+class CheckDigitError(IdError):
+    """Check digit does not match rest of identifer input."""
 
 
-class CheckSumError(IdError): pass
+class CheckSumError(IdError):
+    """The check digit does not equal the check sum."""
 
 
-class CountryCodeError(IdError): pass
+class CountryCodeError(IdError):
+    """Part of the identifier should match an ISO country code."""
 
 
-class LengthError(IdError): pass
-
-
-class NullError(IdError): pass
+class LengthError(IdError):
+    """Invalid length input for identifer."""
